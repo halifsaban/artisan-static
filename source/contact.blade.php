76ea5556d13ccf0da2d176ab138eb2f3d8cdbc12
halifsaban <96216811+halifsaban@users.noreply.github.com>
2022-03-30 17:52:03 +0800

    Update contact.blade.php

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -5,24 +5,25 @@
 @section('content')
     <h1>Contact</h1>
 
-    <form action="https://formcarry.com/s/{{ $page->services->formcarry }}" method="post" netlify>
-        <div>
-            <label for="sender">Name</label><br>
-            <input type="text" name="sender" id="sender" required>
-        </div>
-
-        <div>
-            <label for="email">Email</label><br>
-            <input type="email" name="email" id="email" required>
-        </div>
-
-        <div>
-            <label for="message">Message</label><br>
-            <textarea name="message" id="message" required></textarea>
-        </div>
-
-        <input type="text" name="_gotcha" style="display: none;">
-
-        <input type="submit" name="submit" value="Send">
+    <form name="contact" method="POST" data-netlify="true">
+        <p>
+            <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+            <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+            <label>Your Role: <select name="role[]" multiple>
+                    <option value="leader">Leader</option>
+                    <option value="follower">Follower</option>
+                </select></label>
+        </p>
+        <p>
+            <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+            <button type="submit">Send</button>
+        </p>
     </form>
+
 @endsection
