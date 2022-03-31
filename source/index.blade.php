e35e39b2f1d97b26a34d6551eaa4e354686ec2e5
halifsaban <96216811+halifsaban@users.noreply.github.com>
2022-03-31 10:19:48 +0800

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -5,6 +5,8 @@
 
     <p>Artisan Static is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
 
+    <p>This is for demo!</p>
+
     <p>This comes with code highlighting, share buttons, comments, analytics, an RSS feed, a contact form, a CMS and more.</p>
 
     <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
